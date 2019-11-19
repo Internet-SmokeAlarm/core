484a3a9a12ab3276c639fc989eeacbfaa1e37672
Vale Tolpegin <valetolpegin@gmail.com>
2019-11-19 13:41:19 -0500

    Bump configuration

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ mkdir build
 
 sam build
 
-sam package --template-file template.yaml --s3-bucket fmlaas-core --output-template-file build/packaged.yaml
+sam package --s3-bucket fmlaas-core --output-template-file build/packaged.yaml
 
 sam deploy --template-file build/packaged.yaml --stack-name fmlaas-core --capabilities CAPABILITY_NAMED_IAM
