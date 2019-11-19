165e183b21ae9e651c1932b37a74e3595ce8d107
Vale Tolpegin <valetolpegin@gmail.com>
2019-11-19 13:41:29 -0500

    Update aggregate models function

--- aggregate_models/README.md
+++ aggregate_models/README.md
@@ -1,3 +1,3 @@
 # Aggregate Models
 
-curl -X POST -H "Content-Type: application/json" -d '{"round_id" : 15785926002308826036}' http://localhost:3000/api/v1/round/aggregate
+curl -X POST -H "Content-Type: application/json" -d '{"round_id" : ROUND_ID, "group_id" : GROUP_ID}' http://localhost:3000/api/v1/round/aggregate/pytorch
--- aggregate_models/app.py
+++ aggregate_models/app.py
@@ -1,123 +1,80 @@
 import json
-import boto3
-import os
 from tempfile import NamedTemporaryFile
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-class FashionMNISTCNN(nn.Module):
-
-    def __init__(self, num_outputs=10):
-        super(FashionMNISTCNN, self).__init__()
-
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 16, kernel_size=5, padding=2),
-            nn.BatchNorm2d(16),
-            nn.ReLU(),
-            nn.MaxPool2d(2))
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=5, padding=2),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.MaxPool2d(2))
-
-        self.fc = nn.Linear(7*7*32, num_outputs)
-
-    def forward(self, x):
-        x = self.layer1(x)
-        x = self.layer2(x)
-
-        x = x.view(x.size(0), -1)
-
-        x = self.fc(x)
-
-        return x
-
-def average_nn_parameters(parameters):
-    """
-    Averages passed parameters.
-
-    :param parameters: nn model named parameters
-    :type parameters: list
-    """
-    new_params = {}
-    for name in parameters[0].keys():
-        new_params[name] = sum([param[name].data for param in parameters]) / len(parameters)
-
-    return new_params
+from fmlaas import download_s3_object
+from fmlaas import upload_s3_object
+from fmlaas_pytorch import combine_models
+from fmlaas_pytorch import scale_model
+from fmlaas_pytorch.serde import deserialize_state_dict
+from fmlaas_pytorch.serde import serialize_numpy
+from fmlaas_pytorch.storage import DiskModelStorage
+from fmlaas import get_group_table_name_from_env
+from fmlaas import DynamoDBInterface
+from fmlaas import FLGroup
 
 def load_model_from_s3(object_name):
-    BUCKET_NAME = os.environ["MODELS_BUCKET"]
-
-    s3 = boto3.client('s3')
     model_file = NamedTemporaryFile(delete=True)
 
-    parameters = {}
+    state_dict = None
     try:
         with open(model_file.name, 'wb') as f:
-            s3.download_fileobj(BUCKET_NAME, object_name, f)
+            download_s3_object(object_name, f)
 
-            net = FashionMNISTCNN(num_outputs=10)
-            net.load_state_dict(torch.load(model_file))
-
-            parameters = dict(net.named_parameters())
+        with open(model_file.name, 'r') as f:
+            deserialized_model = DiskModelStorage.load_model(f)
+            state_dict = deserialize_state_dict(deserialized_model)
     finally:
         model_file.close()
 
-    return parameters
-
-def generate_averaged_model(averaged_parameters):
-    net = FashionMNISTCNN(num_outputs=10)
-    with torch.no_grad():
-        params = dict(net.named_parameters())
-        for name in averaged_parameters.keys():
-            params[name].set_(averaged_parameters[name])
-
-    return net
+    return state_dict
 
 def save_model_to_s3(object_name, model):
-    BUCKET_NAME = os.environ["MODELS_BUCKET"]
-
-    s3 = boto3.client('s3')
     model_file = NamedTemporaryFile(delete=True)
     try:
-        with open(model_file.name, 'wb') as f:
-            torch.save(model.state_dict(), f)
+        with open(model_file.name, 'w') as f:
+            serializeable_state_dict = serialize_numpy(model)
+            DiskModelStorage.store_model(f, serializeable_state_dict)
 
         with open(model_file.name, 'rb') as f:
-            s3.upload_fileobj(f, BUCKET_NAME, object_name)
+            upload_s3_object(object_name, f)
     finally:
         model_file.close()
 
-def get_models_from_s3(round_id):
+def generate_global_model(models):
     """
-    :param round_id: int
+    :param models: list(torch.nn)
     """
-    TABLE_NAME = os.environ["LEARNING_ROUND_TABLE_NAME"]
+    global_model = None
 
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table(TABLE_NAME)
+    for model in models:
+        if global_model is None:
+            global_model = load_model_from_s3(model)
+        else:
+            new_model = load_model_from_s3(model)
+            global_model = combine_models(global_model, new_model)
 
-    round_item = table.get_item(Key={"ID" : round_id})["Item"]
-
-    model_parameters = []
-    for model_update_info in round_item["model_updates"]:
-        model_parameters.append(load_model_from_s3(model_update_info["model"]))
-
-    return model_parameters
+    return global_model
 
 def lambda_handler(event, context):
     req_json = json.loads(event.get('body'))
+    group_id = req_json["group_id"]
     round_id = req_json["round_id"]
 
     # TODO : Authenticate user
 
-    models = get_models_from_s3(round_id)
-    averaged_parameters = average_nn_parameters(models)
-    averaged_model = generate_averaged_model(averaged_parameters)
-    save_model_to_s3(str(round_id), averaged_model)
+    dynamodb_ = DynamoDBInterface(get_group_table_name_from_env())
+    group = FLGroup.load_from_db(group_id, dynamodb_)
+
+    models = group.get_models(round_id)
+    num_models = len(models)
+
+    global_model = generate_global_model(models)
+    scaled_global_model = scale_model(global_model, num_models)
+
+    save_model_to_s3(str(round_id), scaled_global_model)
+    group.set_round_global_model(round_id, str(round_id))
+
+    FLGroup.save_to_db(group, dynamodb_)
 
     return {
         "statusCode" : 200,
