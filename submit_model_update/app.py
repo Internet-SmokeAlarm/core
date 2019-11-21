a2a663f92254f733f6b59311f89023725d097bcd
Vale Tolpegin <valetolpegin@gmail.com>
2019-11-21 13:43:34 -0500

    Don't add model to DB in request to submit model

--- submit_model_update/app.py
+++ submit_model_update/app.py
@@ -1,8 +1,5 @@
 import json
 
-from fmlaas import get_group_table_name_from_env
-from fmlaas import DynamoDBInterface
-from fmlaas import FLGroup
 from fmlaas import create_presigned_post
 from fmlaas import generate_model_object_name
 from fmlaas import get_models_bucket_name
@@ -22,13 +19,6 @@ def lambda_handler(event, context):
     model_object_name = generate_model_object_name(device_id, round_id)
     presigned_url = create_presigned_post(get_models_bucket_name(), model_object_name,
                                           FIELDS, CONDITIONS, expiration=EXPIRATION_SEC)
-
-    dynamodb_ = DynamoDBInterface(get_group_table_name_from_env())
-    group = FLGroup.load_from_db(group_id, dynamodb_)
-
-    group.add_model_to_round(round_id, model_object_name)
-
-    FLGroup.save_to_db(group, dynamodb_)
 
     return {
         "statusCode" : 200,
